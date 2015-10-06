e79b43d7621fc35eb5ff213097b8be1e257ae629
coops07 <coops_2121@Hotmail.com>
2015-10-06 14:19:51 -0400

    Create arithmetic.sh

---
+++ arithmetic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#this script demonstrates doing arithmetic
+read -p "Enter a first number please" firstnum
+read -p "Enter a first number please" firstnum
+#do some artithmetic on those num
+sum=$(firstnum + secondnum))
+echo "$firstnum plus $secondnum is $sum"
+sum=$(firstnum - secondnum))
+echo "$firstnum minus $secondnum is $sum"
+sum=$(firstnum * secondnum))
+echo "$firstnum times $secondnum is $sum"
+sum=$(firstnum / secondnum))
+echo "$firstnum divded by $secondnum is $sum"
+sum=$(firstnum % secondnum))
+echo "$firstnum divided by $secondnum leaves a remainder of $sum"
