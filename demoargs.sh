a4841cd1c2432157f45d291e4c389b544b9d488c
Coops07 <coops_2121@hotmail.com>
2015-10-06 19:01:59 +0000

    new progs

---
+++ demoargs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# this script demonstates using positional parameters
+# also known as command line options and arguments
+
+progname=$(basename $0)
+echo "The script command was $progname"
+echo "The command line had '$1' as the first argument"
+echo "The command line had '$2' as the second argument"
